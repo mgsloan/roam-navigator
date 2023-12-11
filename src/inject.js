c33e4eef77da336decaf8ab71e003e8f1fe1957e
Michael Sloan <mgsloan@gmail.com>
2023-12-10 21:31:13 -0700

    Update to extension manifest v3

--- src/inject.js
+++ src/inject.js
@@ -1,6 +1,16 @@
 // Injects the main script. This is necessary in order to bind a
 // keyhandler.
 
+const browser = window.browser || window.chrome;
+function inject(scriptName) {
+  const scriptEl = document.createElement('script');
+  scriptEl.setAttribute('src', browser.runtime.getURL(scriptName));
+  // In testing doesn't seem to be necessary, but may lead to more
+  // predictable execution order.
+  scriptEl.setAttribute('defer', 'defer');
+  document.getElementsByTagName('body')[0].appendChild(scriptEl);
+}
+
 chrome.storage.sync.get(['settings'], (result) => {
   let settings = {};
   if (result.settings) {
@@ -8,11 +18,10 @@ chrome.storage.sync.get(['settings'], (result) => {
   }
 
   const settingsScriptEl = document.createElement('script');
+  settingsScriptEl.setAttribute('defer', 'defer');
   settingsScriptEl.innerText =
     'roamNavigatorSettings = ' + JSON.stringify(settings) + ';';
   document.body.appendChild(settingsScriptEl);
 
-  const scriptEl = document.createElement('script');
-  scriptEl.setAttribute('src', chrome.extension.getURL('roam-navigator.js'));
-  document.body.appendChild(scriptEl);
+  inject('roam-navigator.js');
 });
